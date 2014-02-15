fadde18944e85c87d0439dd2f6d89593e6f2b0fc
Nirmal Utwani <nirmalutwani@seas098.wlan.seas.upenn.edu>
2014-02-15 04:25:10 -0500

    Trying to add dropbox API

---
+++ src/Summarizer/MainFunction.java
@@ -0,0 +1,14 @@
+package Summarizer;
+
+import java.util.ArrayList;
+
+public class MainFunction {
+	public static void main(String args[])
+	{
+
+	}
+	public ArrayList<String> getImportantStatements(ArrayList<String> links, String userName, String password, ArrayList<String> words, boolean marked, int minutes)
+			{
+				return null;				
+			}
+}
